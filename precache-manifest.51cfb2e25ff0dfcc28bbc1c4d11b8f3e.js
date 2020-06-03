c08b034c6d310f48c1fc71a482ac0dab800a5d8f
AngelFox24 <curilaurente@gmail.com>
2020-06-03 15:51:53 -0500

    Updates

---
+++ precache-manifest.51cfb2e25ff0dfcc28bbc1c4d11b8f3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf2344aae2c00c6b8309b0f2a74b5a3",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "304b21f36c287d909378",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/my-app/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304b21f36c287d909378",
+    "url": "/my-app/static/js/main.0c7d99d7.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
